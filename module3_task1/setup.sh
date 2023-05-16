5d84bdfde6503d44f287aedea6ad294789376d48
LCobham <lucascobham@hotmail.com>
2023-05-16 12:45:44 -0300

    Changes in setup.sh

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-apt-get update && apt-get install -y hugo make
-npm install -g markdown-link-check
-npm install -g markdownlint-cli
+## Install correct Hugo version 
+
+sudo -E apt-get update && apt-get install -y wget make
+sudo -E wget "https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb"
+sudo -E dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
+rm -rf hugo_extended_0.84.0_Linux-64bit.deb
+
+## instal markdown linter and check
+
+sudo npm install -g markdown-link-check
+sudo npm install -g markdownlint-cli
+
