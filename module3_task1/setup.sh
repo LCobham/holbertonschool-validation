58fe796f31ba4f74dbc4237c2cf283bb3bbf494a
LCobham <lucascobham@hotmail.com>
2023-05-16 13:08:30 -0300

    Further changes in madule3_task1 setup.sh

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-## Install correct Hugo version 
+## Update and Install make and wget
+sudo apt-get update && sudo apt-get install -y make wget
 
-sudo -E apt-get update && apt-get install -y wget make
-sudo -E wget "https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb"
-sudo -E dpkg -i hugo_extended_0.84.0_Linux-64bit.deb
-rm -rf hugo_extended_0.84.0_Linux-64bit.deb
+## Download Hugo version 0.84 from GitHub
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
 
-## instal markdown linter and check
+## Decompress the executable into "hugo" binary
+tar -xvf hugo_extended_0.109.0_Linux-64bit.tar.gz hugo
 
+## Move "hugo" binary to a directory within the PATH and remove downloaded file
+sudo mv hugo /usr/local/bin/
+rm hugo_extended_0.109.0_Linux-64bit.tar.gz
+
+## Install linter
 sudo npm install -g markdown-link-check
 sudo npm install -g markdownlint-cli
-
